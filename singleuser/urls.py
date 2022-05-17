06d4f1e246d2403012aca43022bc3cc224360223
Dashank Doshi <10doshi12@gmail.com>
2022-05-17 18:37:24 +0530

    fixes again

--- singleuser/urls.py
+++ singleuser/urls.py
@@ -5,7 +5,7 @@ from . views import *
 router = routers.DefaultRouter()
 
 router.register('api/school-view', school_view, basename="school")
-router.register('api/collage-view', college_view, basename="collage")
+router.register('api/college-view', college_view, basename="college")
 router.register('api/employed-view',employed_view, basename="employed")
 router.register('api/configuration-view',configuration_view, basename="configure")
 
