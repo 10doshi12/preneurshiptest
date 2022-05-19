743253cb70f091cb16337438f42679390591c228
Dashank Doshi <10doshi12@gmail.com>
2022-05-19 14:39:00 +0530

    get single companies

--- portfolio/views.py
+++ portfolio/views.py
@@ -37,3 +37,8 @@ class ListPortfolio(viewsets.ViewSet):
         serializer = PortfolioSerializer(queryset, many=True)
         return Response(serializer.data)
 
+    def retrieve(self, request, pk=None):
+        queryset = Portfolio.objects.all()
+        qre = get_object_or_404(queryset, pk=pk)
+        serializer = PortfolioSerializer(qre)
+        return Response(serializer.data)
